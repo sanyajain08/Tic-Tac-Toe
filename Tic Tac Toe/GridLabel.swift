586fe0955cb9d6482015c2f33fb4a1006e69935b
Jean Cho <jeancho@jeans-macbook-air.local>
2018-07-20 11:37:41 -0500

    Initial Commit

---
+++ Tic Tac Toe/GridLabel.swift
@@ -0,0 +1,15 @@
+//
+//  GridLabel.swift
+//  Tic Tac Toe
+//
+//  Created by Jean Cho on 7/5/18.
+//  Copyright © 2018 Jean Cho. All rights reserved.
+//
+
+import UIKit
+
+class GridLabel: UILabel {
+    var canTap = true
+    
+
+}
